d148b97ef67e20352dbb8db4b892e84f579fe260
George Grainger <bsvq48@ncc1.clients.dur.ac.uk>
2025-11-30 23:22:56 +0000

    Added Ollama functionality

--- generate_challenge_4_llm_text.sh
+++ generate_challenge_4_llm_text.sh
@@ -17,5 +17,5 @@ echo "OLLAMA ready to receive commands."
 
 ## Send a request to the server for a story about peguins then save this request as a JSON file
 echo "Sending commands..."
-curl --location "http://$SLURM_JOB_NODELIST:$PORT/api/generate" --data '{"model":"phi4-mini","prompt":"$PROMPT","stream":false}' -o $EXPORT_FILE_LOCATION
+curl --location "http://$SLURM_JOB_NODELIST:$PORT/api/generate" --data "{\"model\":\"phi4-mini\",\"prompt\":\"$PROMPT\",\"stream\":false}" -o $EXPORT_FILE_LOCATION
 echo "Response exported tp $EXPORT_FILE_LOCATION."
