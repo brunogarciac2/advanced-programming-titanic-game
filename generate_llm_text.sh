709f2fb813eab5178d9287d490eb823e489354db
George Grainger <bsvq48@ncc1.clients.dur.ac.uk>
2025-12-02 15:52:57 +0000

    Fixed llm prompting and add virtual environment for python to install and run in so guaranteed to run properly independent of system packages

--- generate_llm_text.sh
+++ generate_llm_text.sh
@@ -5,6 +5,6 @@ PROMPT=$2
 EXPORT_FILE_LOCATION=$3
 
 ## Send a request to the server requesting prompt then save this request as a JSON file
-echo "Sending commands..."
-curl --location "http://$SLURM_JOB_NODELIST:$PORT/api/generate" --data "{\"model\":\"phi4-mini\",\"prompt\":\"$PROMPT\",\"stream\":false}" -o $EXPORT_FILE_LOCATION
+echo "Sending prompt to llm [PORT:$PORT]..."
+curl --location "http://$SLURM_JOB_NODELIST:$PORT/api/generate" --data "{\"model\":\"phi4-mini\",\"prompt\":\"$PROMPT\",\"stream\":false}" -s -o $EXPORT_FILE_LOCATION
 echo "Response exported to $EXPORT_FILE_LOCATION."
